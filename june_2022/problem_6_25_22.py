d4417c7fee67fe2574259d15715d784509457045
chasespengler <chasespengler7@gmail.com>
2022-06-28 13:37:07 -0400

    spiral problem solved

--- june_2022/problem_6_25_22.py
+++ june_2022/problem_6_25_22.py
@@ -35,7 +35,75 @@ mat = \
 [[1,  2,  3,  4,  5],
  [6,  7,  8,  9,  10],
  [11, 12, 13, 14, 15],
- [16, 17, 18, 19, 20]]
+ [16, 17, 18, 19, 20],
+ [21, 22, 23, 24, 25]]
+
+def spiral1(mat):
+    up = False
+    right = True
+    cmax = len(mat[0])
+    rmax = len(mat)
+    cmin = 0
+    rmin = 1
+    r = 0
+    i = 0
+
+    while i < len(mat) * len(mat[0]):
+        if right:
+            for c in range(cmin, cmax, 1):
+                #print("r", r, c)
+                print(mat[r][c])
+                i += 1
+            right = not right
+            cmax -= 1
+        if not up:
+            for r in range(rmin, rmax, 1):
+                #print("d", r, c)
+                print(mat[r][c])
+                i += 1
+            up = not up
+            rmax -= 1
+        if not right:
+            for c in range(cmax-1, cmin-1, -1):
+                #print("l", r, c)
+                print(mat[r][c])
+                i += 1
+            right = not right
+            cmin += 1
+        if up:
+            for r in range(rmax-1, rmin-1, -1):
+                #print("u", r, c)
+                print(mat[r][c])
+                i += 1
+            up = not up
+            rmin += 1
+
+def next_vect(vects):
+    new_vects = vects[1:]
+    new_vects.append(vects[0])
+    return new_vects
+
+#Not functional
+def spiral(mat):     
+    printed_mat = []
+    vects = [[1, 0], [0, 1], [-1, 0], [0, -1]]
+    r = 0
+    c = 0
+    print("work")
+    for i in range(len(mat) * len(mat[0])):
+        print(r, c)
+        if mat[r][c] not in printed_mat:
+            if r < len(mat) and c < len(mat[0]) - 1:
+                r += vects[0][1]
+                c += vects[0][0]
+            else:
+                print(vects[0])
+                vects = next_vect(vects)
+                print(vects[0])
+                r += vects[0][1]
+                c += vects[0][0]
+
+spiral1(mat)
 
 # vects = [[0, 1], [1, 0], [0, -1], [1, 0]]
 # rmax = len(mat) - 1
@@ -63,63 +131,6 @@ mat = \
 #         toggle = not toggle
 #         print("switch ", r, " ", c)
 #         r -= 1
-#         c -= 1
-vects = [[1, 0], [0, 1], [-1, 0], [0, -1]]
-def next_vect(vects):
-    new_vects = vects[1:]
-    new_vects.append(vects[0])
-    return new_vects
-
-def spiral1(mat):
-    ind = [0, 0]
-    upanddown = True
-    cmax = len(mat[0]) - 1
-    rmax = len(mat) - 1
-    cmin = 0
-
-    for i in range(len(mat) * len(mat[0])):
-        print(ind[0], ind[1])
-        if upanddown:
-            if cmin <= ind[1] < cmax:
-                ind[1] += vects[0][0]
-            else:
-                upanddown = not upanddown
-                vects = next_vect(vects)
-        if not upanddown:
-            if ind[0] < rmax - 1:
-                ind[0] += vects[0][1]
-            else:
-                upanddown = not upanddown
-                cmin += 1
-                ind[0] += vects[0][1]
-                print("p", ind[0], ind[1])
-                vects = next_vect(vects)
-                ind[1] += vects[0][0]
-
-def spiral(mat):     
-    printed_mat = []
-    vects = [[1, 0], [0, 1], [-1, 0], [0, -1]]
-    r = 0
-    c = 0
-    print("work")
-    for i in range(len(mat) * len(mat[0])):
-        print(r, c)
-        if mat[r][c] not in printed_mat:
-            if r < len(mat) and c < len(mat[0]) - 1:
-                r += vects[0][1]
-                c += vects[0][0]
-            else:
-                print(vects[0])
-                vects = next_vect(vects)
-                print(vects[0])
-                r += vects[0][1]
-                c += vects[0][0]
-
-spiral(mat)
-        
-
-    
-
-        
+#         c -= 1       
 
         
